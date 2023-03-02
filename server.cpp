7e4363f9bf564b565363c5230ca64462f5db9843
PribylovNikita <54896261+PribylovNikita@users.noreply.github.com>
2023-03-02 12:34:21 +0300

    client -- done? server -- wip

--- server.cpp
+++ server.cpp
@@ -7,76 +7,78 @@
 using boost::asio::ip::tcp;
 
 class session
-  : public std::enable_shared_from_this<session>
+        : public std::enable_shared_from_this<session>
 {
 public:
-  session(tcp::socket socket)
-    : socket_(std::move(socket))
-  {
-  }
+    session(tcp::socket socket)
+    : socket_(std::move(socket)) {
+        std::cout << "connection established" << std::endl;
+        std::cout << this << std::endl;
+    }
 
-  void start()
-  {
-    do_read();
-  }
+    void start() {
+        do_read();
+    }
+
+    ~session() {
+        std::cout << "connection lost" << std::endl;
+    }
 
 private:
-  void do_read()
-  {
-    auto self(shared_from_this());
-    socket_.async_read_some(boost::asio::buffer(data_, max_length),
-        [this, self](boost::system::error_code ec, std::size_t length)
-        {
-          if (!ec)
-          {
-            do_write(length);
-          }
+    void do_read()
+    {
+        //std::cout << "do_read" << std::endl;
+        auto self(shared_from_this());
+        socket_.async_read_some(boost::asio::buffer(data_, max_length),
+                                [this, self](boost::system::error_code ec, std::size_t length) {
+            if (!ec) {
+                do_write(length);
+            }
         });
-  }
+        //std::cout << "do_read end" << std::endl;
+    }
 
-  void do_write(std::size_t length)
-  {
-    auto self(shared_from_this());
-    boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
-        [this, self](boost::system::error_code ec, std::size_t /*length*/)
-        {
-          if (!ec)
-          {
-            do_read();
-          }
+    void do_write(std::size_t length) {
+        //std::cout << "do_write" << std::endl;
+        auto self(shared_from_this());
+        boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
+                               [this, self](boost::system::error_code ec, std::size_t /*length*/) {
+            if (!ec) {
+                do_read();
+            }
         });
-  }
+        //std::cout << "do_write end" << std::endl;
+    }
 
-  tcp::socket socket_;
-  enum { max_length = 1024 };
-  char data_[max_length];
+    tcp::socket socket_;
+    enum { max_length = 1024 };
+    char data_[max_length];
 };
 
 class server
 {
 public:
-  server(boost::asio::io_context& io_context, short port)
-    : acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
-  {
-    do_accept();
-  }
+    server(boost::asio::io_context& io_context, short port)
+    : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)) {
+        do_accept();
+    }
 
 private:
-  void do_accept()
-  {
-    acceptor_.async_accept(
-        [this](boost::system::error_code ec, tcp::socket socket)
-        {
-          if (!ec)
-          {
-            std::make_shared<session>(std::move(socket))->start();
-          }
+    void do_accept() {
+        std::cout << "do_accept" << std::endl;
+        acceptor_.async_accept([this](boost::system::error_code ec, tcp::socket socket) {
+            if (!ec) {
+                auto p_session = std::make_shared<session>(std::move(socket));
+                std::cout << p_session << " session" << std::endl;
+                p_session->start();
+            }
 
-          do_accept();
+            do_accept();
         });
-  }
+        std::cout << "do_accept end" << std::endl;
+    }
 
-  tcp::acceptor acceptor_;
+    tcp::acceptor acceptor_;
 };
 
 int main(int argc, char* argv[])
